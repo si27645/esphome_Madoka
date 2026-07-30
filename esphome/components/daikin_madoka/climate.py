5c89e663364767e5d503fa36db8cca4604641be9
Nuno Silva <33075025+si27645@users.noreply.github.com>
2026-07-30 23:34:58 +0100

    Refactor import statements and update CONFIG_SCHEMA

--- esphome/components/daikin_madoka/climate.py
+++ esphome/components/daikin_madoka/climate.py
@@ -1,18 +1,28 @@
 import esphome.codegen as cg
-from esphome.components import ble_client, climate
 import esphome.config_validation as cv
+
+from esphome.components import ble_client, climate
 from esphome.const import CONF_ID
 
 CODEOWNERS = ["@Petapton"]
 DEPENDENCIES = ["ble_client"]
 
 daikin_madoka_ns = cg.esphome_ns.namespace("daikin_madoka")
+
 DaikinMadoka = daikin_madoka_ns.class_(
-    "DaikinMadoka", climate.Climate, ble_client.BLEClientNode, cg.PollingComponent
+    "DaikinMadoka",
+    climate.Climate,
+    ble_client.BLEClientNode,
+    cg.PollingComponent,
 )
 
 CONFIG_SCHEMA = (
-    climate.climate_schema(DaikinMadoka)
+    climate.CLIMATE_SCHEMA
+    .extend(
+        {
+            cv.GenerateID(): cv.declare_id(DaikinMadoka),
+        }
+    )
     .extend(ble_client.BLE_CLIENT_SCHEMA)
     .extend(cv.polling_component_schema("10s"))
 )
@@ -20,6 +30,7 @@ CONFIG_SCHEMA = (
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
+
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
     await ble_client.register_ble_node(var, config)
